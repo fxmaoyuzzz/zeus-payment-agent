6ec76e625581b7334405b20f289538963a0a309f
lizenghai <lizenghai@yofish.com>
2026-08-25 15:08:56 +0800

    Initialize payment agent project

---
+++ src/main/java/com/moyu/zeuspaymentagent/ZeusPaymentAgentApplication.java
@@ -0,0 +1,13 @@
+package com.moyu.zeuspaymentagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusPaymentAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusPaymentAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moyu/zeuspaymentagent/ZeusPaymentAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyu.zeuspaymentagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusPaymentAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
